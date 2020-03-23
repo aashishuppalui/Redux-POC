67db3a33727ded73a20ccd37f20a9040bbf909e5
auppal <aashish.uppal@eronmicro.com>
2020-03-23 15:58:59 +0530

    redux poc usinf mapStateToProps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { connect } from "react-redux";
 
-function App() {
+function App(props) {
+  console.log(props);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I am App component</h1>
+      <h1>My name is {props.myname}</h1>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    myname: state.name
+  };
+};
+
+export default connect(mapStateToProps)(App);
---
+++ src/reducers/reducer.js
@@ -0,0 +1,10 @@
+const iState = {
+  name: "Ramesh",
+  wishes: ["eat", "code"]
+};
+
+const reducer = (state = iState, action) => {
+  return state;
+};
+
+export default reducer;
